d590a2fbac831f082492e8e0b2d23e8d9244cb00
Stefan Mandel <mandelst@gmail.com>
2015-11-08 08:36:49 +0100

    wildcard string matcher

--- README.md
+++ README.md
@@ -11,9 +11,12 @@ conmatch is a java hamcrest library usable for unit test. The original idea was 
   * no object should equal `null`
   * no object should equal an object of anothe class
   * every object should equal `this`
+* `EnumMatcher` asserts that the argument is an `enum` and `valueOf` is at least once called 
 
 * `PrimitiveArrayMatcher` allows to match primitive (e.g. int, double, char) arrays
 * `MapMatcher` allows to match multiple entries in a map
+
+* `WildcardStringMatcher` allows to match strings containing wildcard patterns (? = single wildcard, * = multiple wildcards)
 
 * `ExceptionMatcher` allows to match an exception by class, name and cause 
 
@@ -28,6 +31,6 @@ Maven Dependency
 <dependency>
 	<groupId>com.github.almondtools</groupId>
 	<artifactId>conmatch</artifactId>
-	<version>0.1.0</version>
+	<version>0.1.1</version>
 </dependency>
 ```
---
+++ src/main/java/com/almondtools/conmatch/strings/WildcardStringMatcher.java
@@ -0,0 +1,46 @@
+package com.almondtools.conmatch.strings;
+
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class WildcardStringMatcher extends TypeSafeMatcher<String> {
+
+	private String pattern;
+
+	public WildcardStringMatcher(String pattern) {
+		this.pattern = pattern;
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("contains ").appendValue(pattern);
+	}
+
+	@Override
+	protected boolean matchesSafely(String item) {
+		StringTokenizer t = new StringTokenizer(pattern, "?*", true);
+		StringBuilder buffer = new StringBuilder();
+		while (t.hasMoreTokens()) {
+			String nextToken = t.nextToken();
+			if ("?".equals(nextToken)) {
+				buffer.append(".");
+			} else if ("*".equals(nextToken)) {
+				buffer.append(".*?");
+			} else {
+				buffer.append(Pattern.quote(nextToken));
+			}
+		}
+		Pattern p = Pattern.compile(buffer.toString());
+		Matcher m = p.matcher(item);
+		return m.find();
+	}
+
+	public static WildcardStringMatcher containsPattern(String pattern) {
+		return new WildcardStringMatcher(pattern);
+	}
+
+}
