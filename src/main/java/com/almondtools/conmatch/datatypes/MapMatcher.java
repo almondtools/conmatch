5707ffda5638ce825c9605fa9ee3fb996c012f9e
Stefan Mandel <Stefan.Mandel@gmail.com>
2016-06-14 07:21:15 +0200

    better map matcher

--- src/main/java/com/almondtools/conmatch/datatypes/MapMatcher.java
+++ src/main/java/com/almondtools/conmatch/datatypes/MapMatcher.java
@@ -1,57 +1,172 @@
 package com.almondtools.conmatch.datatypes;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsNull;
 
 public class MapMatcher<K,V> extends TypeSafeMatcher<Map<K,V>>{
 	
-	private Map<K,V> entries;
+	private Class<K> key;
+	private Class<V> value;
+	private Map<Matcher<K>,Matcher<V>> entries;
 	
-	public MapMatcher() {
-		entries = new LinkedHashMap<>();
+	public MapMatcher(Class<K> key, Class<V> value) {
+		this.key = key;
+		this.value = value;
+		this.entries = new LinkedHashMap<>();
 	}
 
 	public MapMatcher<K,V> entry(K key, V value) {
+		return entry(match(key, this.key), match(value, this.value));
+	}
+
+	public MapMatcher<K,V> entry(Matcher<K> key, V value) {
+		return entry(key, match(value, this.value));
+	}
+
+	public MapMatcher<K,V> entry(K key, Matcher<V> value) {
+		return entry(match(key, this.key), value);
+	}
+
+	public MapMatcher<K,V> entry(Matcher<K> key, Matcher<V> value) {
 		entries.put(key, value);
 		return this;
+	}
+	
+	private <T> Matcher<T> match(T element, Class<T> clazz) {
+		if (element == null) {
+			return nullValue(clazz);
+		} else {
+			return equalTo(element);
+		}
 	}
 
 	@Override
 	public void describeTo(Description description) {
 		description.appendValue(entries);
 	}
+	
+	@Override
+	protected void describeMismatchSafely(Map<K, V> item, Description mismatchDescription) {
+		List<Entry<Matcher<K>, Matcher<V>>> unmatched = new LinkedList<>(entries.entrySet());
+		List<Entry<Matcher<K>, Matcher<V>>> matched = new LinkedList<>();
+		List<Entry<K, V>> notfound = new LinkedList<>();
+		
+		for (Entry<K, V> entry : item.entrySet()) {
+		
+			boolean success = tryMatch(unmatched, matched, entry);
+			if (!success) {
+				notfound.add(new AbstractMap.SimpleEntry<>(entry.getKey(), entry.getValue()));
+			}
+		}
+		
+		if (!unmatched.isEmpty()) {
+			mismatchDescription.appendText("missing entries ").appendValue(map(unmatched));
+		}
+		if (!unmatched.isEmpty() && !notfound.isEmpty()) {
+			mismatchDescription.appendText(", ");
+		}
+		if (!notfound.isEmpty()) {
+			mismatchDescription.appendText("unmatched entries ").appendValue(mapValues(notfound));
+		}
+	}
+
+	private Map<Matcher<K>, Matcher<V>> map(List<Entry<Matcher<K>, Matcher<V>>> entries) {
+		Map<Matcher<K>, Matcher<V>> map = new HashMap<>();
+		for (Entry<Matcher<K>, Matcher<V>> entry : entries) {
+			map.put(entry.getKey(), entry.getValue());
+		}
+		return map;
+	}
+
+	private Map<String, String> mapValues(List<Entry<K, V>> entries) {
+		Matcher<K> keyMatcher = bestKeyMatcher();
+		Matcher<V> valueMatcher = bestValueMatcher();
+		Map<String, String> map = new HashMap<>();
+		for (Entry<K, V> entry : entries) {
+			String key = descriptionOf(keyMatcher, entry.getKey());
+			String value = descriptionOf(valueMatcher, entry.getValue());
+			map.put(key, value);
+		}
+		return map;
+	}
+
+	private Matcher<V> bestValueMatcher() {
+		for (Matcher<V> matcher : entries.values()) {
+			if (matcher.getClass() != IsNull.class) {
+				return matcher;
+			}
+		}
+		return equalTo(null);
+	}
+
+	private Matcher<K> bestKeyMatcher() {
+		for (Matcher<K> matcher : entries.keySet()) {
+			if (matcher.getClass() != IsNull.class) {
+				return matcher;
+			}
+		}
+		return equalTo(null);
+	}
+
+	private <T> String descriptionOf(Matcher<T> valueMatcher, T value) {
+		StringDescription description = new StringDescription();
+		valueMatcher.describeMismatch(value, description);
+		return description.toString();
+	}
 
 	@Override
 	protected boolean matchesSafely(Map<K, V> item) {
-		if (item.size() != entries.size()) {
-			return false;
-		}
-		for (Map.Entry<K, V> entry : item.entrySet()) {
-			K key = entry.getKey();
-			V value = entry.getValue();
-			
-			V expectedValue = entries.get(key);
-			if (expectedValue == null && value == null) {
-				continue;
-			} else if (expectedValue == null && value != null) {
-				return false;
-			} else if (!expectedValue.equals(value)) {
+		List<Entry<Matcher<K>, Matcher<V>>> unmatched = new LinkedList<>(entries.entrySet());
+		List<Entry<Matcher<K>, Matcher<V>>> matched = new LinkedList<>();
+		
+		for (Entry<K, V> entry : item.entrySet()) {
+		
+			boolean success = tryMatch(unmatched, matched, entry);
+			if (!success) {
 				return false;
 			}
 		}
 		
-		return true;
+		return unmatched.isEmpty();
+	}
+
+	private boolean tryMatch(List<Entry<Matcher<K>, Matcher<V>>> unmatched, List<Entry<Matcher<K>, Matcher<V>>> matched, Entry<K, V> entry) {
+		K key = entry.getKey();
+		V value = entry.getValue();
+
+		Iterator<Entry<Matcher<K>, Matcher<V>>> matchers = unmatched.iterator();
+		while (matchers.hasNext()) {
+			Entry<Matcher<K>, Matcher<V>> matcher = matchers.next();
+			if (matcher.getKey().matches(key) && matcher.getValue().matches(value)) {
+				matchers.remove();
+				matched.add(matcher);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static <K,V> MapMatcher<K, V> noEntries(Class<K> key, Class<V> value) {
-		return new MapMatcher<>();
+		return new MapMatcher<>(key, value);
 	}
 
 	public static <K,V> MapMatcher<K, V> containsEntries(Class<K> key, Class<V> value) {
-		return new MapMatcher<>();
+		return new MapMatcher<>(key, value);
 	}
 
 }
---
+++ src/test/java/com/almondtools/conmatch/datatypes/MapMatcherTest.java
@@ -0,0 +1,190 @@
+package com.almondtools.conmatch.datatypes;
+
+import static com.almondtools.conmatch.datatypes.MapMatcher.containsEntries;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+
+public class MapMatcherTest {
+
+	@Test
+	public void testMatchesSafelyWithValues() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry("foo", "bar").entry("08", "15").matchesSafely(map), is(true));
+		assertThat(containsEntries(String.class, String.class).entry("08", "15").entry("foo", "bar").matchesSafely(map), is(true));
+	}
+
+	@Test
+	public void testMatchesSafelyWithoutValues() throws Exception {
+		Map<String, String> map = map();
+
+		assertThat(containsEntries(String.class, String.class).matchesSafely(map), is(true));
+		assertThat(MapMatcher.noEntries(String.class, String.class).matchesSafely(map), is(true));
+	}
+
+	@Test
+	public void testMatchesSafelyWithKeyMatcher() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry(equalTo("foo"), "bar").entry(equalTo("08"), "15").matchesSafely(map), is(true));
+		assertThat(containsEntries(String.class, String.class).entry(equalTo("08"), "15").entry(equalTo("foo"), "bar").matchesSafely(map), is(true));
+	}
+
+	@Test
+	public void testMatchesSafelyWithValueMatcher() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry("foo", equalTo("bar")).entry("08", equalTo("15")).matchesSafely(map), is(true));
+		assertThat(containsEntries(String.class, String.class).entry("08", equalTo("15")).entry("foo", equalTo("bar")).matchesSafely(map), is(true));
+	}
+
+	@Test
+	public void testMatchesSafelyWithMixedMatcher() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry(equalTo("foo"), "bar").entry("08", equalTo("15")).matchesSafely(map), is(true));
+		assertThat(containsEntries(String.class, String.class).entry(equalTo("08"), "15").entry("foo", equalTo("bar")).matchesSafely(map), is(true));
+	}
+
+	@Test
+	public void testMatchesSafelyWithNulls() throws Exception {
+		Map<String, String> map = map("foo", null, null, "15");
+
+		assertThat(containsEntries(String.class, String.class).entry("foo", (String) null).entry((String) null, "15").matchesSafely(map), is(true));
+	}
+
+	@Test
+	public void testMatchesSafelyWithValuesFails() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry("foo", "foo").entry("08", "15").matchesSafely(map), is(false));
+		assertThat(containsEntries(String.class, String.class).entry("bar", "bar").entry("08", "15").matchesSafely(map), is(false));
+	}
+
+	@Test
+	public void testMatchesSafelyWithoutValuesFails() throws Exception {
+		Map<String, String> map = map("foo", "bar");
+
+		assertThat(containsEntries(String.class, String.class).matchesSafely(map), is(false));
+		assertThat(MapMatcher.noEntries(String.class, String.class).matchesSafely(map), is(false));
+	}
+
+	@Test
+	public void testMatchesSafelyWithKeyMatcherFails() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry(equalTo("bar"), "bar").entry("08", "15").matchesSafely(map), is(false));
+		assertThat(containsEntries(String.class, String.class).entry(equalTo("foo"), "foo").entry("08", "15").matchesSafely(map), is(false));
+	}
+
+	@Test
+	public void testMatchesSafelyWithValueMatcherFails() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry("bar", equalTo("bar")).entry("08", "15").matchesSafely(map), is(false));
+		assertThat(containsEntries(String.class, String.class).entry("foo", equalTo("foo")).entry("08", "15").matchesSafely(map), is(false));
+	}
+
+	@Test
+	public void testMatchesSafelyDoubleFailure() throws Exception {
+		Map<String, String> map = map("foo", "bar", "08", "15");
+
+		assertThat(containsEntries(String.class, String.class).entry("bar", equalTo("bar")).entry(equalTo("09"), "15").matchesSafely(map), is(false));
+		assertThat(containsEntries(String.class, String.class).entry("foo", equalTo("foo")).entry("08", equalTo("16")).matchesSafely(map), is(false));
+	}
+
+	@Test
+	public void testDescribeTo() throws Exception {
+		StringDescription description = new StringDescription();
+		
+		containsEntries(String.class, String.class).entry("foo", "bar").entry("08", "15").describeTo(description);
+		
+		assertThat(description.toString(), equalTo("<{\"foo\"=\"bar\", \"08\"=\"15\"}>"));
+	}
+
+	@Test
+	public void testDescribeMismatchEntry() throws Exception {
+		StringDescription description = new StringDescription();
+		
+		containsEntries(String.class, String.class).entry("foo", "bar").entry("08", "15").describeMismatch(map("foo","bar","47","11"), description);
+		
+		assertThat(description.toString(), equalTo("missing entries <{\"08\"=\"15\"}>, unmatched entries <{was \"47\"=was \"11\"}>"));
+	}
+
+	@Test
+	public void testDescribeMismatchUnmatched() throws Exception {
+		StringDescription description = new StringDescription();
+		
+		containsEntries(String.class, String.class).entry("foo", "bar").entry("08", "15").describeMismatch(map("foo","bar","08","15","47","11"), description);
+		
+		assertThat(description.toString(), equalTo("unmatched entries <{was \"47\"=was \"11\"}>"));
+	}
+
+	@Test
+	public void testDescribeMismatchMissing() throws Exception {
+		StringDescription description = new StringDescription();
+		
+		containsEntries(String.class, String.class).entry("foo", "bar").entry("08", "15").describeMismatch(map("foo","bar"), description);
+		
+		assertThat(description.toString(), equalTo("missing entries <{\"08\"=\"15\"}>"));
+	}
+
+	@Test
+	public void testDescribeMismatchMissingOnEmpty() throws Exception {
+		StringDescription description = new StringDescription();
+		
+		containsEntries(String.class, String.class).describeMismatch(map("47","11"), description);
+		
+		assertThat(description.toString(), equalTo("unmatched entries <{was \"47\"=was \"11\"}>"));
+	}
+
+	@Test
+	public void testDescribeMismatchUnmatchedWithCustomMatcher() throws Exception {
+		StringDescription description = new StringDescription();
+		
+		containsEntries(String.class, String.class).entry(custom("foo"), custom("bar")).entry(custom("08"), custom("15")).describeMismatch(map("foo","bar","08","15","47","11"), description);
+		
+		assertThat(description.toString(), equalTo("unmatched entries <{custom \"47\"=custom \"11\"}>"));
+	}
+	
+
+	private Matcher<String> custom(String string) {
+		return new IsEqual<String>(string) {
+			@Override
+			public void describeMismatch(Object item, Description description) {
+				description.appendText("custom ").appendValue(item);
+			}
+		};
+	}
+
+	private Map<String, String> map(String... elements) {
+		Map<String, String> map = new HashMap<String, String>();
+		String key = null;
+		boolean nullValue = false;
+		for (String element : elements) {
+			if (key == null && !nullValue) {
+				if (element == null) {
+					nullValue = true;
+				} else {
+					key = element;
+				}
+			} else {
+				map.put(key, element);
+				key = null;
+				nullValue = false;
+			}
+		}
+		return map;
+	}
+
+}
