d98158e4f8895026a7b1de908dea24664d987989
Stefan Mandel <mandelst@gmail.com>
2016-12-23 13:18:07 +0100

    supporting primitive array matching out of order

--- src/main/java/com/almondtools/conmatch/datatypes/PrimitiveArrayMatcher.java
+++ src/main/java/com/almondtools/conmatch/datatypes/PrimitiveArrayMatcher.java
@@ -11,28 +11,29 @@ import com.almondtools.conmatch.util.SimpleClass;
 public class PrimitiveArrayMatcher<T> extends TypeSafeMatcher<T> {
 
 	private T array;
+	private boolean anyOrder;
 
 	private PrimitiveArrayMatcher(T array) {
 		this.array = array;
 	}
 
-	public static Matcher<boolean[]> booleanArrayContaining(boolean... items) {
+	public static PrimitiveArrayMatcher<boolean[]> booleanArrayContaining(boolean... items) {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
-	public static Matcher<char[]> charArrayContaining(char... items) {
+	public static PrimitiveArrayMatcher<char[]> charArrayContaining(char... items) {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
-	public static Matcher<byte[]> byteArrayContaining(byte... items) {
+	public static PrimitiveArrayMatcher<byte[]> byteArrayContaining(byte... items) {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
-	public static Matcher<short[]> shortArrayContaining(short... items) {
+	public static PrimitiveArrayMatcher<short[]> shortArrayContaining(short... items) {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
-	public static Matcher<int[]> intArrayContaining(int... items) {
+	public static PrimitiveArrayMatcher<int[]> intArrayContaining(int... items) {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
@@ -40,7 +41,7 @@ public class PrimitiveArrayMatcher<T> extends TypeSafeMatcher<T> {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
-	public static Matcher<long[]> longArrayContaining(long... items) {
+	public static PrimitiveArrayMatcher<long[]> longArrayContaining(long... items) {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
@@ -48,12 +49,51 @@ public class PrimitiveArrayMatcher<T> extends TypeSafeMatcher<T> {
 		return new PrimitiveArrayMatcher<>(items);
 	}
 
+	public PrimitiveArrayMatcher<T> inAnyOrder() {
+		Class<?> type = array.getClass().getComponentType();
+		if (type == boolean.class) {
+			sort((boolean[]) array);
+		} else if (type == char.class) {
+			Arrays.sort((char[]) array);
+		} else if (type == byte.class) {
+			Arrays.sort((byte[]) array);
+		} else if (type == short.class) {
+			Arrays.sort((short[]) array);
+		} else if (type == int.class) {
+			Arrays.sort((int[]) array);
+		} else if (type == float.class) {
+			Arrays.sort((float[]) array);
+		} else if (type == long.class) {
+			Arrays.sort((long[]) array);
+		} else if (type == double.class) {
+			Arrays.sort((double[]) array);
+		}
+
+		this.anyOrder = true;
+		return this;
+	}
+
+	private void sort(boolean[] array) {
+		int trueValues = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i]) {
+				trueValues++;
+			}
+		}
+		for (int i = 0; i < trueValues; i++) {
+			array[i] = true;
+		}
+		for (int i = trueValues; i < array.length; i++) {
+			array[i] = false;
+		}
+	}
+
 	@Override
 	public void describeTo(Description description) {
 		description.appendText("an array containing values of type ")
-			.appendValue(array.getClass().getComponentType())
-			.appendText(": ")
-			.appendValue(array);
+				.appendValue(array.getClass().getComponentType())
+				.appendText(": ")
+				.appendValue(array);
 	}
 
 	@Override
@@ -82,24 +122,88 @@ public class PrimitiveArrayMatcher<T> extends TypeSafeMatcher<T> {
 		}
 		Class<?> type = array.getClass().getComponentType();
 		if (type == boolean.class) {
-			return Arrays.equals((boolean[]) array, (boolean[]) item);
+			return Arrays.equals((boolean[]) array, processed((boolean[]) item));
 		} else if (type == char.class) {
-			return Arrays.equals((char[]) array, (char[]) item);
+			return Arrays.equals((char[]) array, processed((char[]) item));
 		} else if (type == byte.class) {
-			return Arrays.equals((byte[]) array, (byte[]) item);
+			return Arrays.equals((byte[]) array, processed((byte[]) item));
 		} else if (type == short.class) {
-			return Arrays.equals((short[]) array, (short[]) item);
+			return Arrays.equals((short[]) array, processed((short[]) item));
 		} else if (type == int.class) {
-			return Arrays.equals((int[]) array, (int[]) item);
+			return Arrays.equals((int[]) array, processed((int[]) item));
 		} else if (type == float.class) {
-			return Arrays.equals((float[]) array, (float[]) item);
+			return Arrays.equals((float[]) array, processed((float[]) item));
 		} else if (type == long.class) {
-			return Arrays.equals((long[]) array, (long[]) item);
+			return Arrays.equals((long[]) array, processed((long[]) item));
 		} else if (type == double.class) {
-			return Arrays.equals((double[]) array, (double[]) item);
+			return Arrays.equals((double[]) array, processed((double[]) item));
 		} else {
 			return false;
 		}
+	}
+
+	private boolean[] processed(boolean[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			sort(item);
+		}
+		return item;
+	}
+
+	private char[] processed(char[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
+	}
+
+	private byte[] processed(byte[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
+	}
+
+	private short[] processed(short[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
+	}
+
+	private int[] processed(int[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
+	}
+
+	private float[] processed(float[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
+	}
+
+	private long[] processed(long[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
+	}
+
+	private double[] processed(double[] item) {
+		if (anyOrder) {
+			item = Arrays.copyOf(item, item.length);
+			Arrays.sort(item);
+		}
+		return item;
 	}
 
 }
